7c2e825b355e5ce317529ad3b3f2873885901731
baggiop1 <baggiop1@udayton.edu>
2024-10-22 22:49:00 -0400

    Tried to fix errors

--- code/LibraryManagementSystem/src/tests/BookTest.java
+++ code/LibraryManagementSystem/src/tests/BookTest.java
@@ -1,8 +1,6 @@
 package tests;
 
 import com.example.Book;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class BookTest {
     // Create private Book object
